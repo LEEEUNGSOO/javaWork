59590af4ccbfe14dd2d5de3051723503ac92dfbe
이응수 <sistsay@gmail.com>
2025-08-05 14:14:55 +0900

    arrayIndexOB

--- src/com/multi/exception/ExceptionEx1.java
+++ src/com/multi/exception/ExceptionEx1.java
@@ -6,10 +6,32 @@ public class ExceptionEx1 {
         str=str.charAt(0)!='A'?null:str;
         System.out.println(str.startsWith("Ajax"));
     }
-    public static void main(String[] args) {
-        ExceptionEx1 obj=new  ExceptionEx1();
-        obj.nullPointerExceptionExample1("SpringLesson");
+    public void arrayIndexOutOfBoundsExceptionExample1(){
+        int []m=new int[5];
+        for(int i=0;i<=5;i++){
+            try {// 도전코드....
+                m[i]=i;
+            }catch (ArrayIndexOutOfBoundsException e){//예외발생 구간~
+                System.out.println("ArrayIndexOutOfBoundsException~~~");
+                e.printStackTrace();
+                /*
+                printStackTrace로 인한 메시지
+                java.lang.ArrayIndexOutOfBoundsException: Index 5 out of bounds for length 5
+	            at com.multi.exception.ExceptionEx1.arrayIndexOutOfBoundsExceptionExample1(ExceptionEx1.java:13)
+	            at com.multi.exception.ExceptionEx1.main(ExceptionEx1.java:24)
+                 */
+            }finally {//꼭
+                System.out.println("finally~~~");
+                
+            }
+        }
 
     }
+    public static void main(String[] args) {
+        ExceptionEx1 obj=new  ExceptionEx1();
+        //obj.nullPointerExceptionExample1("SpringLesson");
+        obj.arrayIndexOutOfBoundsExceptionExample1();
 
+
+    }
 }
