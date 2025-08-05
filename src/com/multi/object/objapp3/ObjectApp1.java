bd6d0a381b0b7c27971692005c504644f2bd0f49
이응수 <sistsay@gmail.com>
2025-08-05 11:53:16 +0900

    object instance

--- src/com/multi/object/objapp3/ObjectApp1.java
+++ src/com/multi/object/objapp3/ObjectApp1.java
@@ -18,10 +18,16 @@ public class ObjectApp1   extends Object implements Cloneable {
 //    }
 
     public void appMethod1(){
+        Object obj=new String("abc");//UpCasting
+        //System.out.println(obj instanceof String);//obj를 StringType으로 downcast가능하면 true
+    Object obj2=new Object();
+    String str=(String) obj2;
+     //   System.out.println(str instanceof  String);
 
     }
     public static void main(String[] args) {
         ObjectApp1 obj=new  ObjectApp1();
+        obj.appMethod1();
 
 
     }
