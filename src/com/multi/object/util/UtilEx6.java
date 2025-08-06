c28b3238bea1ec3d7b59acd61b17ab21e151b2dd
이응수 <sistsay@gmail.com>
2025-08-06 17:11:28 +0900

    util

---
+++ src/com/multi/object/util/UtilEx6.java
@@ -0,0 +1,23 @@
+package com.multi.object.util;
+
+import java.util.*;
+
+public class UtilEx6 {
+    public static void main(String[] args) {
+
+
+    int[] numbers = {3, 1, 4, 1, 5};
+
+    // 정렬
+        Arrays.sort(numbers);//1 1 3 4 5
+        System.out.println("Sorted: " + Arrays.toString(numbers));
+
+    // 이진 검색
+    int index = Arrays.binarySearch(numbers, 4);
+        System.out.println("Index of 4: " + index);
+
+    // 배열 복사
+    int[] copied = Arrays.copyOf(numbers, 10); // 길이 7로 복사
+        System.out.println("Copied: " + Arrays.toString(copied));
+ }
+}
---
+++ src/com/multi/object/util/UtilEx7.java
@@ -0,0 +1,24 @@
+package com.multi.object.util;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class UtilEx7 {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);// list 고정데이터로 변환 추가 ,수정 삭제 불가
+        System.out.println(list);
+        //list.add(6); error발생 Arrays.asList (고정형으로 변환되어서)
+        //섞기
+        Collections.shuffle(list);
+        System.out.println(list);
+        // 역순
+        Collections.reverse(list);
+        System.out.println(list);
+        // 최대값/최소값
+        System.out.println("Max: " + Collections.max(list));
+        System.out.println("Min: " + Collections.min(list));
+
+
+    }
+}
