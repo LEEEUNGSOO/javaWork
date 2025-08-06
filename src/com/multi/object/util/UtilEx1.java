b9a94f6e53a63559008aa6148e9fc4fef5a57a23
이응수 <sistsay@gmail.com>
2025-08-06 10:18:38 +0900

    string

---
+++ src/com/multi/object/util/UtilEx1.java
@@ -0,0 +1,15 @@
+package com.multi.object.util;
+//util과 관련된 class
+//String,StringBuffer,StringBuilder
+//Object( 기능은 모든 객체의 공통기능이다)
+//Collection,Thread
+public class UtilEx1 {
+
+    public void stringMaker1(){
+
+    }
+    public static void main(String[] args) {
+        UtilEx1 obj=new  UtilEx1();
+
+    }
+}
